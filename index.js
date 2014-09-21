f2ddf8a4704051abea9b9889128e2aeabd5c2f37
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-21 22:12:21 +0100

    adds directory structure. adds index,js. adds static file. adds procfile. adds gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+var path = require('path');
+var Joi = require('joi');
+var rs = require('./lib/modules/routes.js');
+
+var config= { };
+var server = Hapi.createServer(process.env.PORT || 8080, config);
+
+server.route(rs);
+
+server.start(function(){
+  console.log("Server started: " + server.info.uri);
+});
+
+module.exports = server;
