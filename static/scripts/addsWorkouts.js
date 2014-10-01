daf56c83008b0aef14610bd8fdb74d98c47ea22b
MMendes999 <mario.c.mendes1985@gmail.com>
2014-10-01 11:26:35 +0100

    updates workoutform.html. updates addsWorkouts.js

--- static/scripts/addsWorkouts.js
+++ static/scripts/addsWorkouts.js
@@ -6,29 +6,11 @@ $(document).ready(function() {
     console.log ('Button add-set clicked');
 
         var htmlSets = '<div class="sets">' +
-            '<label for="reps" class="labels">Reps</label><input type="text" id="reps" class="reps-column" placeholder="How many reps?" />' +
-            '<label for="kilos" class="labels">Kg\'s</label><input type="text" id="kilos" class="kilos-column" placeholder="How much Kg?" />' +
+            '<label for="reps" class="labels">Reps</label><input type="text" name="reps" class="reps-column" placeholder="How many reps?" />' +
+            '<label for="kilos" class="labels">Kg\'s</label><input type="text" name="kilos" class="kilos-column" placeholder="How much Kg?" />' +
         '</div>';
 
-    $(htmlSets).insertAfter('div.sets:last');
-  });
-
-  // function to add more exercise
-  $('#add-exercise').on('click', function() {
-    console.log('Button add-exercise clicked');
-
-        var htmlExercise = '<div class="workouts">' +
-            '<label for="exercise" class="labels"> <strong>Exercise</strong> </label><input type="text" name="exercise" id="exercise" placeholder="Which exercise?" autofocus />' +
-            '<label for="musclegroup" class="labels"> <strong>Muscle-Group</strong> </label><input type="text" name="musclegroup" id="musclegroup" placeholder="Which muscle-group?" />' +
-
-            '<div class="sets">' +
-                '<label for="reps" class="labels">Reps</label><input type="text" name="reps" id="reps" class="reps-column" placeholder="How many reps?" />' +
-                '<label for="kilos" class="labels">Kg\'s</label><input type="text" name="kilos" id="kilos" class="kilos-column" placeholder="How much Kg?" />' +
-            '</div>' +
-            '<hr>' +
-        '</div>';
-
-    $(htmlExercise).insertAfter('div.workouts:last');
+    $('div.sets:last').append(htmlSets);
   });
 
 });
