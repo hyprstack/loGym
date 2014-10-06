bc54bdca2230adc34f8cd25d37e658c64a709df6
MMendes999 <mario.c.mendes1985@gmail.com>
2014-10-06 21:11:08 +0100

    updates routes.js schema. updates docList

--- static/scripts/addsWorkouts.js
+++ static/scripts/addsWorkouts.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
     console.log ('Button add-set clicked');
 
         var htmlSets = '<div class="sets" name="set">' +
-            '<label for="reps" class="labels">Reps</label><input type="text" name="reps" class="reps-column" placeholder="How many reps?" />' +
-            '<label for="kilos" class="labels">Kg\'s</label><input type="text" name="kilos" class="kilos-column" placeholder="How much Kg?" />' +
+            '<label for="reps" class="labels">Reps</label><input type="number" name="reps" class="reps-column" placeholder="How many reps?" />' +
+            '<label for="kilos" class="labels">Kg\'s</label><input type="number" name="kilos" class="kilos-column" placeholder="How much Kg?" />' +
         '</div>';
 
     $('div.sets:last').append(htmlSets);
