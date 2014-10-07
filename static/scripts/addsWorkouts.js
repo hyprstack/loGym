1db4e7d4e815fbb33813e1479aa2e74fe3a76603
MMendes999 <mario.c.mendes1985@gmail.com>
2014-10-07 16:12:21 +0100

    updates addsWorkouts.js

--- static/scripts/addsWorkouts.js
+++ static/scripts/addsWorkouts.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
   console.log('Document ready');
-  var n = $('div.sets').length;
-  console.log(n);
 
 
   // function to add sets
   $('#add-set').on('click', function() {
+
     console.log ('Button add-set clicked');
 
         var htmlSets = '<div class="sets" name="set">' +
@@ -14,16 +13,25 @@ $(document).ready(function() {
         '</div>';
 
         $('div.sets:last').append(htmlSets);
+        
   });
 
   // function to remove sets
   $('#remove-set').on('click', function(event) {
+
     console.log ('Button remove-set clicked');
     var n = $('div.sets').length;
+
       if ( n === 1) {
+
         event.preventDefault();
+
       } else {
+
           $('div.sets:last').remove();
+
       }
+
     });
+
 });
