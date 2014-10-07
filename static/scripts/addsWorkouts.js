6866b16d081221da2d85988d888d6666addd0d09
MMendes999 <mario.c.mendes1985@gmail.com>
2014-10-07 16:10:03 +0100

    adds css styling. adds remove sets function

--- static/scripts/addsWorkouts.js
+++ static/scripts/addsWorkouts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   console.log('Document ready');
+  var n = $('div.sets').length;
+  console.log(n);
 
 
-
-  // function to add sets to specific exercise
+  // function to add sets
   $('#add-set').on('click', function() {
     console.log ('Button add-set clicked');
 
@@ -12,7 +13,17 @@ $(document).ready(function() {
             '<label for="kilos" class="labels">Kg\'s</label><input type="number" name="kilos" class="kilos-column" placeholder="How much Kg?" />' +
         '</div>';
 
-    $('div.sets:last').append(htmlSets);
+        $('div.sets:last').append(htmlSets);
   });
 
+  // function to remove sets
+  $('#remove-set').on('click', function(event) {
+    console.log ('Button remove-set clicked');
+    var n = $('div.sets').length;
+      if ( n === 1) {
+        event.preventDefault();
+      } else {
+          $('div.sets:last').remove();
+      }
+    });
 });
