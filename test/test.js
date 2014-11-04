ae2d6536a78d0bd8f7a089c37ab334f82d9e3872
MMendes999 <mario.c.mendes1985@gmail.com>
2014-11-04 21:52:00 +0000

    adds first test

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var lab = require("lab");
+// var lab = exports.lab = lab.script();
+var server = require('../'); // require index.js
+var request = require('request');
+
+
+lab.experiment("Test status response", function() {
+  // tests
+
+  lab.test("Render index.html file as home page", function(done){
+    var options = {
+      method: 'GET',
+      url: '/home',
+      handler: {file: 'index.html'}
+    };
+    //server inject lets you simulate an http request
+    server.inject(options, function(response){
+      lab.expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+});
